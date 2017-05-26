664c4e53d7a423cac4cd50cb46536f496ffa6941
jeffsoto56 <jeffsoto56@gmail.com>
2017-05-26 13:58:18 -0600

    Semana03
    Inicio del Proyecto

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 define('LIBS','libs/'); 
 
 //variables cuando uso localhost
-define('URL','http://localhost:81/ejemplomvc/');
+define('URL','http://localhost/tiroDeEsquina/');
 define('DB_TYPE','mysql');
 define('DB_HOST','localhost');
-define('DB_NAME','bd_app');
+define('DB_NAME','bd_tiroDeEsquina');
 define('DB_USER','root');
 define('DB_PASS','');
 
 /*No cambiar la hash_key porque es la clave del password*/
-define('HASH_GENERAL_KEY','ejemplomvc');
+define('HASH_GENERAL_KEY','tiroDeEsquina');
 
 /*ESTE ES PARA LA BASE DE DATOS SOLAMENTE*/
-define('HASH_PASSWORD_KEY','ejemplomvc');
+define('HASH_PASSWORD_KEY','tiroDeEsquina');
 ?>
